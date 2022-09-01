f169f39d4352e182aeed0d560864d7175cc150c3
Daniel Ugarov <tyradire@yandex.ru>
2022-09-01 20:23:25 +0300

    add swiper

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,63 @@
 const slides = [
   {
     id: 1,
-    image: '../images/image2',
+    image: './images/image9.jpg',
+    description: 'Секции для рубашек',
   },
   {
     id: 2,
-    image: '../images/image3',
+    image: './images/image10.jpg',
+    description: 'Выдвижная штанга',
   },
   {
     id: 3,
-    image: '../images/image4',
+    image: './images/image11.jpg',
+    description: 'Брючницы',
   },
   {
     id: 4,
-    image: '../images/image5',
+    image: './images/image12.jpg',
+    description: 'Пантографы',
   },
   {
     id: 5,
-    image: '../images/image6',
+    image: './images/image13.jpg',
+    description: 'Блоки внешних ящиков',
   },
   {
     id: 6,
-    image: '../images/image7',
+    image: './images/image14.jpg',
+    description: 'ТВ-модуль',
   },
   {
     id: 7,
-    image: '../images/image8',
+    image: './images/image15.jpg',
+    description: 'ТВ-модуль',
+  },
+  {
+    id: 8,
+    image: './images/image16.jpg',
+    description: 'Выдвижные корзины для белья или обуви',
+  },
+  {
+    id: 9,
+    image: './images/image17.jpg',
+    description: 'Секции для рубашек',
+  },
+  {
+    id: 10,
+    image: './images/image18.jpg',
+    description: 'Блоки внутренних ящиков',
+  },
+  {
+    id: 11,
+    image: './images/image19.jpg',
+    description: 'Выдвижные корзины для белья или обуви',
+  },
+  {
+    id: 12,
+    image: './images/image20.jpg',
+    description: 'Выдвижные зеркала внутри шкафа',
   },
 ]
 
@@ -38,6 +70,14 @@ const burgerMenu = document.getElementById('burger-menu');
 const burgerButton = document.getElementById('burger-button');
 const burgerLines = document.querySelectorAll('.burger-menu__line')
 
+const singleSlider = document.getElementById('single-slider');
+const sliderDescription = document.querySelector('.single-slider__description');
+const sliderImage = singleSlider.querySelector('.single-slider__image');
+const singleLeft = document.getElementById('single-left');
+const singleRight = document.getElementById('single-right');
+
+console.log(sliderDescription.textContent)
+
 const openBurgerMenu = () => {
   burgerMenu.classList.toggle('burger-menu__opened_open');
   burgerLines.forEach(elem => elem.classList.toggle('burger-menu__cross'));
@@ -46,6 +86,7 @@ const openBurgerMenu = () => {
 burgerButton.addEventListener('click', openBurgerMenu);
 
 let slide = 0;
+let swipe = 0;
 
 const slideLeft = () => {
   // if (slide = slide + 820;) {
@@ -88,3 +129,26 @@ function scrollFunction() {
     header.style.backgroundColor = "rgba(255, 255, 255, 0.8)";
   }
 }
+
+const leftSingleSwap = () => {
+  if (swipe === 0) {
+    return
+  } else {
+    swipe = swipe - 1;
+    sliderDescription.textContent = slides[swipe].description;
+    sliderImage.src = slides[swipe].image;
+  }
+}
+
+const rightSingleSwap = () => {
+  if (swipe === 11) {
+    swipe = 1
+  } else {
+    swipe = swipe + 1;
+  }
+  sliderDescription.textContent = slides[swipe].description;
+  sliderImage.src = slides[swipe].image;
+}
+
+singleLeft.addEventListener('click', leftSingleSwap);
+singleRight.addEventListener('click', rightSingleSwap);
