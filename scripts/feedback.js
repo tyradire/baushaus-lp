71dab0229fcea9243b6658c2a8ac32b29b5176db
Daniel Ugarov <tyradire@yandex.ru>
2022-09-02 23:51:07 +0300

    add form php

--- scripts/feedback.js
+++ scripts/feedback.js
@@ -1,12 +1,10 @@
 const formSubmit = document.getElementById('feedback-form');
-const nameForm = document.getElementById('name');
-const emailForm = document.getElementById('email');
-const phoneForm = document.getElementById('phone');
 
 formSubmit.onsubmit = function(){
   var http = new XMLHttpRequest();
   http.open("POST", "contacts.php", true);
   http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  console.log(this.namecl.value, this)
   http.send("namecl=" + this.namecl.value +"&phonecl=" + this.phonecl.value + "&emailcl=" + this.emailcl.value + "&textcl=" + this.textcl.value );
   http.onreadystatechange = function() {
     if (http.readyState == 4 && http.status == 200) {
